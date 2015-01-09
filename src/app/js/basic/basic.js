6cb9b4bbc77e3c353a4ca3a759d50fa783103f5e
Senthil Premkumar <senthil.premkumar@servicenow.com>
2015-01-09 23:46:15 +0100

    Add remove/add css class

--- src/app/js/basic/basic.js
+++ src/app/js/basic/basic.js
@@ -1,27 +1,46 @@
 (function () {
 	"use strict";
 
+	var $c = {
+		hasClass: function (elem, className) {
+			return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+		},
+		addClass: function (elem, className) {
+			if (!this.hasClass(elem, className)) {
+				elem.className += ' ' + className;
+			}
+		},
+		removeClass: function (elem, className) {
+			var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, ' ') + ' ';
+			if (this.hasClass(elem, className)) {
+				while (newClass.indexOf(' ' + className + ' ') >= 0) {
+					newClass = newClass.replace(' ' + className + ' ', ' ');
+				}
+				elem.className = newClass.replace(/^\s+|\s+$/g, '');
+			}
+		}
+	};
+
 	var module = {};
 
 	module.controller = function () {
 		this.message = "Hello World from Mithril!";
 	};
 
+
 	var eventHandlers = {
 		onmouseover: function (event) {
-			event.toElement.className += " " + "selected";
+			$c.addClass(event.toElement, "selected");
 		},
 		onmouseout: function (event) {
-			event.fromElement.className = event.fromElement.className.replace(/(?:^|\s)selected(?!\S)/, '');
+			$c.removeClass(event.fromElement, 'selected');
 		}
 	};
-
-	var classNames = {class: 'initial'};
 
 	module.getView = function (ctrl) {
 		var arrView = [];
 		for (var i = 0, len = 10; i < len; i++) {
-			arrView.push(m('div.initial',eventHandlers, ctrl.message + "-" + i));
+			arrView.push(m('div.initial', eventHandlers, ctrl.message + "-" + i));
 		}
 		return arrView;
 	};
